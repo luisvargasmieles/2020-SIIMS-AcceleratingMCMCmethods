e8769731eea8613e6466352f4cb10702a1289d73
Luis Vargas Mieles <67636941+luis-vargas-mieles@users.noreply.github.com>
2020-07-29 12:22:30 +0100

    Create SKROCK_tomography.py

---
+++ Python_code/Tomography_reconstruction/SKROCK_tomography.py
@@ -0,0 +1,10 @@
+"""
+TOMOGRAPHIC IMAGE RECONSTRUCTION                     %
+We implement the SK-ROCK algorithm described in: "Accelerating
+Proximal Markov Chain Monte Carlo by Using an Explicit Stabilized
+Method", Marcelo Pereyra, Luis Vargas Mieles, and Konstantinos C.
+Zygalakis, SIAM Journal on Imaging Sciences, Vol. 13, No. 2, 2020
+Permalink: https://doi.org/10.1137/19M1283719 
+
+@author: Luis Vargas Mieles
+"""
